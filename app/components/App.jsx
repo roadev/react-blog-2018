88d9691b3aea0da12f75ea3353006a8d34a9da90
Juan Roa <jdroa92@gmail.com>
2017-12-15 18:44:12 -0500

    Initial commit

---
+++ app/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Home from './Home/Home';
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      {/* <Route path="another_route" exact component={AnotherComponent} /> */}
+    </Switch>
+  </Router>
+);
+
+export default App;
---
+++ app/components/Nav/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      {/* <li>
+        <Link to="/another_route">Another Component</Link>
+      </li> */}
+    </ul>
+  </nav>
+);
+
+export default Nav;
